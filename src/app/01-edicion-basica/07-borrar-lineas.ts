7402b5c2862bd10b600bddd2b3d499498d7854a8
SebastianAguirre17 <sebaaguirre2012@gmail.com>
2020-10-10 00:02:37 -0300

    Curso VsCode

---
+++ src/app/01-edicion-basica/07-borrar-lineas.ts
@@ -0,0 +1,27 @@
+/*
+    Objetivo:
+        Comentar un bloque de código
+
+    Tips:
+        ⇧ ⌘ K
+        Ctrl + Shift + K
+
+    PRO:
+        Seleccionar todas las ocurrencias de la selección
+        ⇧ ⌘ L
+        Ctrl + Shift + L
+*/
+
+let NoMeBorres = ':)';
+
+NoMeBorres = '1';
+NoMeBorres = '1';
+Borrame2    = 'a';
+
+
+// Resultado final (sin los comentarios claro.)
+
+// let NoMeBorres = ':)';
+
+// NoMeBorres = '1';
+// NoMeBorres = '1';
